dbeb7dbdc56bbb397395f74f5ba037ad81276835
Diego Reyes <diegorele13@gmail.com>
2025-12-09 03:58:06 -0500

    feat(users): add users service with create method

---
+++ src/services/users.service.ts
@@ -0,0 +1,14 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+import APIBase from './httpBase'
+
+export interface CreateUserBody extends Record<string, unknown> {}
+
+class UsersService extends APIBase {
+  async create<T>(body: CreateUserBody, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    return this.post<T>('users', body, undefined, config)
+  }
+}
+
+const usersService = new UsersService()
+
+export default usersService
